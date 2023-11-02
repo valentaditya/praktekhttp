2933b66ec4f084b26889ddfb8befeb81c096e547
valentaditya <149642029+valentaditya@users.noreply.github.com>
2023-11-02 09:08:39 +0700

    server.js

---
+++ server.js
@@ -0,0 +1,63 @@
+const http = require('http');
+
+
+const todos =[
+    {id:1, text: 'Todo One'},
+    {id:2, text: 'Todo Two'},
+    {id:3, text: 'Todo Three'},
+]
+ 
+const server = http.createServer((req, res) => {
+
+
+    const {method,url}= req;
+    let body =[];
+    
+
+    req
+        .on('data', chunk => {
+            body.push(chunk);
+        })
+        .on('end', () => {
+            body = Buffer.concat(body).toString();
+            
+            let status =404;
+            const respose ={
+                success:false,
+                results:[],
+                error:''
+            }
+
+            if (method === 'GET' && url === '/todos'){
+                
+                status = 200;
+                respose.success = true;
+                respose.results= todos;
+
+            }else if (method === 'POST' && url === '/todos'){
+                const {id, text } = JSON.parse(body);
+
+                if (!id || !text) {
+                    status = 400;
+                    respose.error = 'Please add id and  text';
+                }else {
+                    todos.push({id, text});
+                    status =201;
+                    respose.success =true;
+                    respose.results= todos;
+                }
+            }
+            res.writeHead(status,{
+                'Content-Type' : 'application/json',
+               'X-Powered-By' : 'Node.js'
+            });
+            res.end(JSON.stringify(respose));
+
+        });
+        
+   
+});
+
+const PORT = 5003;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
